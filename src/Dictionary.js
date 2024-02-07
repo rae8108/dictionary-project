7a0f064d5a553ad05f12afdafdf3ec5f59befbd5
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-02-07 12:28:27 +0000

    added the photos api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,34 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
-
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "MeMlcwgzsrSPVkTZNRTCglNnnCuhoVgSw4eV6w2Ar7QDI7sv6f7gh16d";
+    let pexelsApiUrl = ` https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -37,6 +51,7 @@ export default function Dictionary() {
         <div className="hint"> suggested words: sunset, forest, hike</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
